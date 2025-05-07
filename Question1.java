bcafacc313e4f3fd3178176945da4fecb695c79f
Akaayush14 <akaayush40@gmail.com>
2025-05-07 12:52:47 +0545

    File name changed

--- Question1.java
+++ Question1.java
@@ -1,29 +1,58 @@
 //1
 //WAP to find if a number is prime or composite:
+// import java.util.Scanner;
+// public class Question1{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter a number: ");
+//         int n = sc.nextInt();
+
+//         if (n <= 1) {
+//             System.out.println(n + " is neither prime nor composite.");
+//         } 
+//         else {
+//             boolean prime = true;
+//             //n is being divided by zero then n is not a prime number.
+//             for (int i = 2; i <= n / 2; i++) {
+//                 if (n % i == 0) {
+//                     prime = false;
+//                     break;
+//                 }
+//             }
+//             if (prime) {
+//                 System.out.println(n + " is a prime number.");
+//             } else {
+//                 System.out.println(n + " is a composite number.");
+//             }
+//         }
+//     }
+// }
+
 import java.util.Scanner;
 public class Question1{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.println("Enter a number: ");
         int n = sc.nextInt();
 
-        if (n <= 1) {
-            System.out.println(n + " is neither prime nor composite.");
-        } 
+        if (n <= 1){
+            System.out.println(n + "is not prime nor composite");
+        }
         else {
             boolean prime = true;
-            //n is being divided by zero then n is not a prime number.
-            for (int i = 2; i <= n / 2; i++) {
-                if (n % i == 0) {
+            for (int i=2; i<=n; i++){
+                if(n%i == 0){
                     prime = false;
                     break;
                 }
             }
-            if (prime) {
-                System.out.println(n + " is a prime number.");
-            } else {
-                System.out.println(n + " is a composite number.");
+            if (prime){
+                System.out.println(n + "is a prime number");
+            }else{
+                System.out.println(n + "is not prime nor composite");
             }
         }
+
     }
+
 }
--- Question3.java
+++ Question3.java
@@ -13,18 +13,24 @@
 // }
 
 //Next way:using for loop
+// public class Question3{
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 5; i++) {
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
 public class Question3{
-    public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= i; j++) {
+    public static void main(String[] args){
+        for (int i=1; i<5; i++){
+            for (int j=1; j <= i; j++){
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 }
-
-
-
-
 
